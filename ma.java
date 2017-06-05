03baa82fe25c4bac7fd6aa668170c3583cace163
Romanmani <manikandancse14@gmail.com>
2017-06-05 15:35:11 +0530

    Create ma.java

---
+++ ma.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ma
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a%2=0)
+{
+Sysem.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
