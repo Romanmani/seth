38c6bd4df66c65c82914caa5018debcc288174e9
Romanmani <manikandancse14@gmail.com>
2017-06-05 15:04:42 +0530

    Create README.java

---
+++ README.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class m
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+{
+System.out.println("positive no");
+}
+else if(a<0)
+{
+System.out.println("neg no");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
+
+
