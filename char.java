f925a4b495f9f5e5f857130246fd3b48c0090041
Romanmani <manikandancse14@gmail.com>
2017-06-08 18:14:32 +0530

    Create char.java

--- char.java
+++ char.java
@@ -4,8 +4,8 @@ public class char
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
-String a=s.nextLine();
-char n=Character.toString(a);
+String x=s.nextLine();
+char n=x.charAt(0);
 if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
 {
 System.out.println("vowels");
