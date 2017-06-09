bcd914c1fffa8fca5e5e3731fff4b7b8cf7adf02
Romanmani <manikandancse14@gmail.com>
2017-06-09 09:27:58 -0700

    Create count.java

---
+++ count.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class count
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int count=0;
+for(int i=0;i<n;i++)
+{
+count=count+i;
+System.out,println(count);
+}
+}
+}
