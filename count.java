3441a5f999706d745ada086c35280a1437fa1f9c
Romanmani <manikandancse14@gmail.com>
2017-06-09 09:34:51 -0700

    Create count.java

--- count.java
+++ count.java
@@ -6,10 +6,10 @@ public static void main(String args[])
 Scanner s=new Scanner(System.in);
 int a=s.nextInt();
 int count=0;
-for(int i=0;i<n;i++)
+for(int i=1;i<a;i++)
 {
-count=count+i;
-System.out,println(count);
+count++;
+System.out.println(i);
 }
 }
 }
